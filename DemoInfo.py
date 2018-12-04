57a3c5fc7683ab79d7630fb5bc7359b5f18a84c1
tsablatzky <44913090+tsablatzky@users.noreply.github.com>
2018-12-04 18:56:46 -0500

    Add files via upload

---
+++ DemoInfo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  2 20:50:01 2018
+
+@author: TenleySablatzky
+"""
+
+print("Let's start by getting to know you a little bit.")
+
+Gender = input("Please select one: M/F or type N for nonbinary ")
+str.capitalize("n", "m", "f")
+
+Age = input("Enter your age numberically: ")
+
+Weight = input("What is your current weight in pounds? ")
+
+Height = input("Please enter your height in inches: ")
+
+User = []
+User = [Gender, Age, Weight, Height]
+
